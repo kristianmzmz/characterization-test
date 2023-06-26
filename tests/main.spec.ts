8347bfbbf953c18136f719611a0c617af7f7cd66
Kristian Muñoz <kristian.mzmz@gmail.com>
2023-06-26 09:34:46 +0200

    First iteration: Explore what it does with a plain text Add several tests for the plain text just to be sure we test different string sizes

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,8 +1,14 @@
 import {idothings} from "../src/main";
 
 describe('characterisation testing', () => {
-    it('should not apply any change to a plain string', () => {
-        const aPlainText: string = 'this is a plain text';
+    it.each
+    ([
+        ["this is a plain text"],
+        ["hello world"],
+        ["charmander"],
+        ["characterization tests examples"]
+    ])
+    ('should not apply any change to a plain string', (aPlainText: string) => {
         const expectedResult: string = aPlainText
 
         const result: string = idothings(aPlainText);
